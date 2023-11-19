da05978467404f630613287c1719c9fb625edc19
9itamar9 <9itamar9@gmail.com>
2023-11-19 23:05:49 +0200

    Can parse objects with strings as fields

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use std::{collections::HashMap, error, str::FromStr};
+
 use nom::{
     self,
     branch::alt,
     bytes::complete::{is_not, tag},
     character::complete::multispace0,
-    sequence::{delimited, tuple},
+    combinator::opt,
+    sequence::{delimited, pair, tuple},
 };
 
 fn main() {
@@ -17,12 +20,48 @@ fn main() {
     println!("{a:?}")
 }
 
-fn parse_json_obj(json: &str) -> nom::IResult<&str, &str> {
+#[derive(Debug)]
+enum JsonVal {
+    Str(String),
+    Num(isize),
+    Array(Vec<JsonVal>),
+    Obj(HashMap<String, JsonVal>),
+    Boolean(bool),
+    Null,
+}
+
+// impl FromStr for JsonVal {
+//     type Err = Box<dyn error::Error>;
+//
+//     fn from_str(s: &str) -> Result<Self, Self::Err> {
+//         alt((
+//             parse_json_obj,
+//             parse_json_arr,
+//             parse_json_string,
+//             parse_json_num,
+//         ))?
+//     }
+// }
+//
+// fn parse_json_arr(json: &str) -> nom::IResult<&str, JsonVal> {}
+// fn parse_json_string(json: &str) -> nom::IResult<&str, JsonVal> {}
+// fn parse_json_num(json: &str) -> nom::IResult<&str, JsonVal> {}
+
+fn parse_json_obj(json: &str) -> nom::IResult<&str, JsonVal> {
     let (i, _) = tag("{")(json)?;
     let (i, _) = multispace0(i)?;
-    let (i, _) = nom::multi::separated_list1(alt((tag(","), multispace0)), parse_field)(i)?;
+    let (i, list) = nom::multi::separated_list1(pair(opt(tag(",")), multispace0), parse_field)(i)?;
     let (i, _) = multispace0(i)?;
-    tag("}")(i)
+    tag("}")(i)?;
+    Ok((
+        i,
+        JsonVal::Obj(
+            list.into_iter()
+                .map(|(f, v)| (f.to_string(), JsonVal::Str(v.to_string())))
+                .into_iter()
+                .collect(),
+        ),
+    ))
 }
 
 fn parse_field(i: &str) -> nom::IResult<&str, (&str, &str)> {
