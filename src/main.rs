d56843d38a54f9915ac4479ce327aab1f272112b
9itamar9 <9itamar9@gmail.com>
2023-11-19 22:32:18 +0200

    Can parse json with multiple simple string objects

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use nom::{
     self,
+    branch::alt,
     bytes::complete::{is_not, tag},
     character::complete::multispace0,
     sequence::{delimited, tuple},
@@ -9,7 +10,9 @@ fn main() {
     let print = std::env::args().nth(1).expect("Need json as arg");
     println!("{print}");
     let json = "{             \
-        \"field\": \"1\"    }";
+        \"field\": \"1\",   
+        \"banana\": \"2\"    
+        }";
     let a = parse_json_obj(json);
     println!("{a:?}")
 }
@@ -17,10 +20,16 @@ fn main() {
 fn parse_json_obj(json: &str) -> nom::IResult<&str, &str> {
     let (i, _) = tag("{")(json)?;
     let (i, _) = multispace0(i)?;
+    let (i, _) = nom::multi::separated_list1(alt((tag(","), multispace0)), parse_field)(i)?;
+    let (i, _) = multispace0(i)?;
+    tag("}")(i)
+}
+
+fn parse_field(i: &str) -> nom::IResult<&str, (&str, &str)> {
+    // TODO: Handle escape for \"
     let (i, field) = delimited(tag("\""), is_not("\""), tag("\""))(i)?;
     let (i, _) = tuple((multispace0, tag(":"), multispace0))(i)?;
     let (i, val) = delimited(tag("\""), is_not("\""), tag("\""))(i)?;
-    let (i, _) = multispace0(i)?;
-    let (_, _) = tag("}")(i)?;
-    Ok((val, field))
+    println!("{field}: {val}");
+    Ok((i, (field, val)))
 }
