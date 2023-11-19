a0b35176c88c3324b0ee89eb0f9461f23d9b6113
9itamar9 <9itamar9@gmail.com>
2023-11-19 23:54:14 +0200

    Made string, num and array parser, need to fix array

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,37 @@
-use std::{collections::HashMap, error, str::FromStr};
+use std::collections::HashMap;
 
 use nom::{
     self,
     branch::alt,
     bytes::complete::{is_not, tag},
-    character::complete::multispace0,
-    combinator::opt,
-    sequence::{delimited, pair, tuple},
+    character::complete::{digit1, multispace0},
+    combinator::{map, map_res, opt},
+    multi::separated_list1,
+    number::complete::double,
+    sequence::{delimited, pair, preceded, terminated, tuple},
 };
 
 fn main() {
     let print = std::env::args().nth(1).expect("Need json as arg");
     println!("{print}");
-    let json = "{             \
-        \"field\": \"1\",   
-        \"banana\": \"2\"    
-        }";
-    let a = parse_json_obj(json);
-    println!("{a:?}")
+    // let json = "{             \
+    //     \"field\": \"1\",
+    //     \"banana\": \"2\"
+    //     }";
+    let json = "[\"1\", \"2\"]";
+    let a = alt((
+        parse_json_obj,
+        parse_json_arr,
+        parse_json_num,
+        parse_json_string,
+    ))(json);
+    println!("{a:#?}")
 }
 
 #[derive(Debug)]
 enum JsonVal {
     Str(String),
-    Num(isize),
+    Num(f64),
     Array(Vec<JsonVal>),
     Obj(HashMap<String, JsonVal>),
     Boolean(bool),
@@ -43,32 +51,54 @@ enum JsonVal {
 //     }
 // }
 //
-// fn parse_json_arr(json: &str) -> nom::IResult<&str, JsonVal> {}
-// fn parse_json_string(json: &str) -> nom::IResult<&str, JsonVal> {}
-// fn parse_json_num(json: &str) -> nom::IResult<&str, JsonVal> {}
+//
+fn parse_json_arr(json: &str) -> nom::IResult<&str, JsonVal> {
+    let (i, fields) = preceded(
+        pair(tag("["), multispace0),
+        terminated(
+            separated_list1(pair(opt(tag(",")), multispace0), parse_json_string),
+            pair(multispace0, tag("]")),
+        ),
+    )(json)?;
+    Ok((i, JsonVal::Array(fields.into_iter().collect())))
+}
+
+fn parse_json_string(json: &str) -> nom::IResult<&str, JsonVal> {
+    map(delimited(tag("\""), is_not("\""), tag("\"")), |v: &str| {
+        JsonVal::Str(v.to_string())
+    })(json)
+}
+
+fn parse_json_num(json: &str) -> nom::IResult<&str, JsonVal> {
+    map(double, |d| (JsonVal::Num(d)))(json)
+}
 
 fn parse_json_obj(json: &str) -> nom::IResult<&str, JsonVal> {
-    let (i, _) = tag("{")(json)?;
-    let (i, _) = multispace0(i)?;
-    let (i, list) = nom::multi::separated_list1(pair(opt(tag(",")), multispace0), parse_field)(i)?;
-    let (i, _) = multispace0(i)?;
-    tag("}")(i)?;
+    let (i, fields) = preceded(
+        pair(tag("{"), multispace0),
+        terminated(
+            separated_list1(pair(opt(tag(",")), multispace0), parse_field),
+            pair(multispace0, tag("}")),
+        ),
+    )(json)?;
+
     Ok((
         i,
         JsonVal::Obj(
-            list.into_iter()
-                .map(|(f, v)| (f.to_string(), JsonVal::Str(v.to_string())))
+            fields
+                .into_iter()
+                .map(|(f, v)| (f.to_string(), v))
                 .into_iter()
                 .collect(),
         ),
     ))
 }
 
-fn parse_field(i: &str) -> nom::IResult<&str, (&str, &str)> {
+fn parse_field(i: &str) -> nom::IResult<&str, (&str, JsonVal)> {
     // TODO: Handle escape for \"
     let (i, field) = delimited(tag("\""), is_not("\""), tag("\""))(i)?;
     let (i, _) = tuple((multispace0, tag(":"), multispace0))(i)?;
-    let (i, val) = delimited(tag("\""), is_not("\""), tag("\""))(i)?;
-    println!("{field}: {val}");
+    let (i, val) = parse_json_string(i)?;
+    println!("{field}: {val:#?}");
     Ok((i, (field, val)))
 }
